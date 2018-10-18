f8fc59453f30fff3f2547e108687a64c2d20e879
Vanessa~Roux <vanessa.roux891@orange.fr>
2018-10-18 22:44:34 +0200

    Step 2 majuscule ok + mise en place du champ de recherche pour Step 3

--- public/app.js
+++ public/app.js
@@ -8,24 +8,26 @@
 var memory = [];
 
 var getValue = function getValue() {
+
+    //Récup l'input name
     var valeur = document.getElementById("name").value;
 
     if (valeur == "") {
         valeur = "père noel";
     }
-    memory.push(valeur);
-    document.getElementById("answer").innerHTML = " Bonjour " + valeur + " !";
 
-    var li = document.createElement('li');
-    //valeur = valeur[0].toUpperCase()
-    var letter = valeur.split('');
-    // let letterMap = letter.map((x, i) => {
-    // (i == 0){
-    //     x.toUpperCase()
-    // } 
-    // })
-    console.log(letter);
-    li.innerHTML = valeur;
+    //Met la 1ère lettre du nom en majuscule
+    var letterMaj = valeur.split('');
+    letterMaj[0] = letterMaj[0].toUpperCase();
+    var newWord = letterMaj.join('');
+    memory.push(newWord);
+    console.log(newWord);
+
+    // Affiche le nom récupéré + la majuscule
+    document.getElementById("answer").innerHTML = " Bonjour " + newWord + " !";
+
+    //Crée une liste avec les noms saisis
+    var li = document.createElement("li");
+    li.innerHTML = newWord;
     document.getElementById("list").append(li);
-    console.log(memory);
 };
--- src/app.es6.js
+++ src/app.es6.js
@@ -6,24 +6,26 @@
 let memory = [];
 
 const getValue = () => {
+
+    //Récup l'input name
     let valeur = document.getElementById("name").value;
 
     if (valeur == "") {
         valeur = "père noel"
     }
-    memory.push(valeur)
-    document.getElementById("answer").innerHTML = ` Bonjour ${valeur} !`;
 
-    let li = document.createElement('li')
-    //valeur = valeur[0].toUpperCase()
-    let letter = valeur.split('')
-    // let letterMap = letter.map((x, i) => {
-    // if (i == 0){
-    //     x.toUpperCase()
-    // } 
-    // })
-    console.log(letter)
-    li.innerHTML = valeur
+    //Met la 1ère lettre du nom en majuscule
+    let letterMaj = valeur.split('');
+    letterMaj[0] = letterMaj[0].toUpperCase();
+    let newWord = letterMaj.join('');
+    memory.push(newWord);
+    console.log(newWord);
+
+     // Affiche le nom récupéré + la majuscule
+     document.getElementById("answer").innerHTML = ` Bonjour ${newWord} !`;
+
+    //Crée une liste avec les noms saisis
+    let li = document.createElement("li")
+    li.innerHTML = newWord
     document.getElementById("list").append(li)
-    console.log(memory);
 }
