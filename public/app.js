61146928c5d7490bf9578f3a0590d64a2c1804ea
Vanessa~Roux <vanessa.roux891@orange.fr>
2018-10-18 16:59:09 +0200

    Step 2 galère de majuscules git statusgit status

--- public/app.js
+++ public/app.js
@@ -11,14 +11,21 @@ var getValue = function getValue() {
     var valeur = document.getElementById("name").value;
 
     if (valeur == "") {
-        valeur = "Père Noel";
+        valeur = "père noel";
     }
     memory.push(valeur);
     document.getElementById("answer").innerHTML = " Bonjour " + valeur + " !";
 
     var li = document.createElement('li');
     //valeur = valeur[0].toUpperCase()
-    li.innerHTML = valeur[0].toUpperCase();
+    var letter = valeur.split('');
+    // let letterMap = letter.map((x, i) => {
+    // (i == 0){
+    //     x.toUpperCase()
+    // } 
+    // })
+    console.log(letter);
+    li.innerHTML = valeur;
     document.getElementById("list").append(li);
     console.log(memory);
 };
--- src/app.es6.js
+++ src/app.es6.js
@@ -9,14 +9,21 @@ const getValue = () => {
     let valeur = document.getElementById("name").value;
 
     if (valeur == "") {
-        valeur = "Père Noel"
+        valeur = "père noel"
     }
     memory.push(valeur)
     document.getElementById("answer").innerHTML = ` Bonjour ${valeur} !`;
 
     let li = document.createElement('li')
     //valeur = valeur[0].toUpperCase()
-    li.innerHTML = valeur[0].toUpperCase()
+    let letter = valeur.split('')
+    // let letterMap = letter.map((x, i) => {
+    // if (i == 0){
+    //     x.toUpperCase()
+    // } 
+    // })
+    console.log(letter)
+    li.innerHTML = valeur
     document.getElementById("list").append(li)
     console.log(memory);
 }
