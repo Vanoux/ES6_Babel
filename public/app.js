2b701710ef25e5eab57deadff9dc0f3c1a61d29b
Vanessa~Roux <vanessa.roux891@orange.fr>
2018-10-19 22:57:36 +0200

    Step 3 affichage des noms commençant par la chaîne de caractère ok

--- public/app.js
+++ public/app.js
@@ -5,22 +5,24 @@
 //     document.write(` Bonjour ${name}`);  
 //  }
 
+//Tableau vide pour pusher les nom dedans
 var memory = [];
 
+// Fonction du boutton envoyer 
 var getValue = function getValue() {
 
-    //Récup l'input name
+    //Récup la valeur de l'input name
     var valeur = document.getElementById("name").value;
-
+    //Si la valeur est vide alors ce sera père noêl qui s'affichera
     if (valeur == "") {
         valeur = "père noel";
     }
 
     //Met la 1ère lettre du nom en majuscule
-    var letterMaj = valeur.split('');
-    letterMaj[0] = letterMaj[0].toUpperCase();
-    var newWord = letterMaj.join('');
-    memory.push(newWord);
+    var letterMaj = valeur.split(''); //On "split" le nom récupéré (éclate la string en array)
+    letterMaj[0] = letterMaj[0].toUpperCase(); //On dit que pour la 1ère lettre du nom (indice[0] de l'array), on veut quelle soit égale à majuscule
+    var newWord = letterMaj.join(''); //On "join" les éléments de l'array pour les remettre en string 
+    memory.push(newWord); //Puis on "push" le nom transformé dans le tableau vide
     console.log(newWord);
 
     // Affiche le nom récupéré + la majuscule
@@ -28,6 +30,35 @@ var getValue = function getValue() {
 
     //Crée une liste avec les noms saisis
     var li = document.createElement("li");
-    li.innerHTML = newWord;
+    li.innerHTML = newWord; //On écrit le nom dans la liste
     document.getElementById("list").append(li);
+};
+
+// Fonction du bouton rechercher
+var searchValue = function searchValue() {
+    //Récup la valeur de l'input research
+    var search = document.getElementById("research").value;
+
+    //Si le champs de recherche est vide
+    // if(search == ""){
+    //     search = "Faite une recherche por favor !"
+    // } 
+
+    //Compare le mot recherché avec les mot dans le tableau
+    var letterMaj = search.split(''); //On "split" le nom recherché (éclate la string en array)  
+    letterMaj[0] = letterMaj[0].toUpperCase(); //On dit que pour la 1ère lettre du nom (indice[0] de l'array), on veut quelle soit égale à majuscule
+    var word = letterMaj.join(''); //On "join" les éléments de l'array pour les remettre en string
+    var compare = memory.filter(function (x) {
+        return x.startsWith(word);
+    }); //On "filter" les éléments du tableau avec la condition "startsWith" => booléen qui vérifie le mot cherché avec les mots du tableau
+
+    //Si il n'y a pas de mots dans le tableau correspondant à la recherche
+    if (compare == "") {
+        compare = "Il n'y a aucun résultat pour cette recherche !";
+    }
+    //Affiche le résultat
+    var display = document.getElementById("result");
+    display.innerHTML = compare;
+
+    console.log(compare);
 };
--- src/app.es6.js
+++ src/app.es6.js
@@ -3,22 +3,24 @@
 //     document.write(` Bonjour ${name}`);  
 //  }
 
+//Tableau vide pour pusher les nom dedans
 let memory = [];
 
+// Fonction du boutton envoyer 
 const getValue = () => {
 
-    //Récup l'input name
+    //Récup la valeur de l'input name
     let valeur = document.getElementById("name").value;
-
-    if (valeur == "") {
+    //Si la valeur est vide alors ce sera père noêl qui s'affichera
+    if (valeur == "") { 
         valeur = "père noel"
     }
 
     //Met la 1ère lettre du nom en majuscule
-    let letterMaj = valeur.split('');
-    letterMaj[0] = letterMaj[0].toUpperCase();
-    let newWord = letterMaj.join('');
-    memory.push(newWord);
+    let letterMaj = valeur.split('');          //On "split" le nom récupéré (éclate la string en array)
+    letterMaj[0] = letterMaj[0].toUpperCase(); //On dit que pour la 1ère lettre du nom (indice[0] de l'array), on veut quelle soit égale à majuscule
+    let newWord = letterMaj.join('');          //On "join" les éléments de l'array pour les remettre en string 
+    memory.push(newWord);                      //Puis on "push" le nom transformé dans le tableau vide
     console.log(newWord);
 
      // Affiche le nom récupéré + la majuscule
@@ -26,6 +28,33 @@ const getValue = () => {
 
     //Crée une liste avec les noms saisis
     let li = document.createElement("li")
-    li.innerHTML = newWord
+    li.innerHTML = newWord                     //On écrit le nom dans la liste
     document.getElementById("list").append(li)
+}
+
+// Fonction du bouton rechercher
+const searchValue = () => {
+    //Récup la valeur de l'input research
+    let search = document.getElementById("research").value;
+    
+    //Si le champs de recherche est vide
+    // if(search == ""){
+    //     search = "Faite une recherche por favor !"
+    // } 
+    
+    //Compare le mot recherché avec les mot dans le tableau
+    let letterMaj = search.split('');                     //On "split" le nom recherché (éclate la string en array)  
+    letterMaj[0] = letterMaj[0].toUpperCase();            //On dit que pour la 1ère lettre du nom (indice[0] de l'array), on veut quelle soit égale à majuscule
+    let word = letterMaj.join('');                        //On "join" les éléments de l'array pour les remettre en string
+    let compare = memory.filter(x => x.startsWith(word)); //On "filter" les éléments du tableau avec la condition "startsWith" => booléen qui vérifie le mot cherché avec les mots du tableau
+
+    //Si il n'y a pas de mots dans le tableau correspondant à la recherche
+    if(compare == ""){
+        compare = "Il n'y a aucun résultat pour cette recherche !"
+    }
+    //Affiche le résultat
+    let display = document.getElementById("result");
+    display.innerHTML = compare
+
+    console.log(compare);
 }
